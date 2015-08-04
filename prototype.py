79886d5ff198d04c171730e0e2147f26feac1502
CJ Carey <perimosocordiae@gmail.com>
2015-08-04 16:29:36 -0500

    refactoring main code, pep8 linting

--- prototype.py
+++ prototype.py
@@ -9,49 +9,66 @@ def _main_static(imgfile, debug=False):
   if img is None:
     print 'Error: could not read %s' % imgfile
     return
-  # rescale if neccessary
-  scale = 800./max(img.shape[:2])
-  if scale < 1:
-    img = cv2.resize(img, (0,0), fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
-
-  rects = find_rects(img)
-  attrs = [attributes(crop_card(img, r)) for r in rects]
-
+  img, rects, attrs = process_image(img)
   if debug:
-    print "%s: Found %d cards" % (imgfile, len(rects))
-    cv2.drawContours(img, rects, -1, (0, 255, 0), 3)
-    for rect, attr in zip(rects, attrs):
-      label = ''.join(str(a)[:3].title() for a in attr)
-      pos = rect.min(axis=0)
-      cv2.putText(img, label, tuple(pos+1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-      cv2.putText(img, label, tuple(pos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.imshow('debug view for ' + imgfile, img)
-    cv2.waitKey()
+    show_debug_view(img, rects, attrs, name=imgfile)
   else:
-    for i,j,k in find_sets(attrs):
-      cv2.drawContours(img, (rects[i],rects[j],rects[k]), -1, (0,255,0), 3)
-      break
-    else:
-      pos = (img.shape[0]/2, img.shape[1]/2 - 100)
-      cv2.putText(img, "No sets found", (pos[0]+2,pos[1]+2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-      cv2.putText(img, "No sets found", pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    cv2.imshow('set view for ' + imgfile, img)
-    cv2.waitKey()
+    show_set_view(img, rects, attrs, name=imgfile)
 
 
 def _main_camera(debug=False):
   raise NotImplementedError()
 
 
+def process_image(img):
+  # rescale if necessary
+  scale = 800./max(img.shape[:2])
+  if scale < 1:
+    img = cv2.resize(img, (0, 0), fx=scale, fy=scale,
+                     interpolation=cv2.INTER_AREA)
+
+  rects = find_rects(img)
+  attrs = [attributes(crop_card(img, r)) for r in rects]
+  return img, rects, attrs
+
+
+def show_debug_view(img, rects, attrs, waitkey=None, name=''):
+  print "%s: Found %d cards" % (name, len(rects))
+  cv2.drawContours(img, rects, -1, (0, 255, 0), 3)
+  for rect, attr in zip(rects, attrs):
+    label = ''.join(str(a)[:3].title() for a in attr)
+    pos = rect.min(axis=0)
+    cv2.putText(img, label, tuple(pos+1), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 0, 0))
+    cv2.putText(img, label, tuple(pos), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255))
+  cv2.imshow('debug view for ' + name, img)
+  cv2.waitKey()
+
+
+def show_set_view(img, rects, attrs, waitkey=None, name=''):
+  for i,j,k in find_sets(attrs):
+    cv2.drawContours(img, (rects[i],rects[j],rects[k]), -1, (0,255,0), 3)
+    break
+  else:
+    pos = (img.shape[0]/2, img.shape[1]/2 - 100)
+    cv2.putText(img, "No sets found", (pos[0]+2,pos[1]+2),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.putText(img, "No sets found", pos, cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 0, 255), 2)
+  cv2.imshow('set view for ' + name, img)
+  cv2.waitKey()
+
+
 def crop_card(img, bbox, width=450, height=450):
   bbox = bbox.astype(np.float32)
   h = np.array([[0,0],[width-1,0],[width-1,height-1],[0,height-1]], np.float32)
   transform = cv2.getPerspectiveTransform(bbox, h)
-  return cv2.warpPerspective(img, transform, (width,height))
+  return cv2.warpPerspective(img, transform, (width, height))
 
 
 def find_sets(attributes):
-  for i,j,k in combinations(xrange(len(attributes)), 3):
+  for i, j, k in combinations(xrange(len(attributes)), 3):
     for items in zip(attributes[i],attributes[j],attributes[k]):
       if len(set(items)) not in (1,3):
         break
@@ -66,7 +83,8 @@ def attributes(card):
   # threshold out the shapes
   thresh = cv2.inRange(hsv, (min_hue,min_sat,0),(max_hue,255,255))
 
-  _, contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  _, contours, hier = cv2.findContours(thresh, cv2.RETR_TREE,
+                                       cv2.CHAIN_APPROX_SIMPLE)
   outer_mask = hier[0,:,-1] < 0
   filling = card_filling(outer_mask)
   contours = [c for i,c in enumerate(contours) if outer_mask[i]]
@@ -127,10 +145,12 @@ def angle_cos(contour):
   return np.abs((d1*d2).sum(axis=1) / np.sqrt((d1*d1).sum(axis=1)*(d2*d2).sum(axis=1)))
 
 
-def find_rects(img, min_val=220, max_sat=70, side_err_scale=0.02, min_area=1000,  max_corner_angle_cos=0.3):
+def find_rects(img, min_val=220, max_sat=70, side_err_scale=0.02, min_area=1000,
+               max_corner_angle_cos=0.3):
   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   thresh = cv2.inRange(hsv, (0,0,min_val),(255,max_sat,255))
-  _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+  _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST,
+                                    cv2.CHAIN_APPROX_SIMPLE)
   rects = []
   for cnt in contours:
     side_err_thresh = side_err_scale * cv2.arcLength(cnt, True)
